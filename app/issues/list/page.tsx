0ea00376d0736c0f1609f35bd38559e623f3e088
ZRNOF <ZRNOF@protonmail.com>
2024-04-04 17:07:38 +0800

    filter issue

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -4,9 +4,22 @@ import { IssueStatusBadge, Link } from "@/app/components"
 import prisma from "@/prisma/client"
 import { Table } from "@radix-ui/themes"
 import IssueActions from "./IssueActions"
+import { Status } from "@prisma/client"
 
-const IssuesPage = async () => {
-	const issues = await prisma.issue.findMany()
+interface IssuesPageProps {
+	searchParams: { status: Status }
+}
+
+const IssuesPage = async ({ searchParams }: IssuesPageProps) => {
+	const statuses = Object.values(Status)
+	const status = statuses.includes(searchParams.status)
+		? searchParams.status
+		: undefined
+	const issues = await prisma.issue.findMany({
+		where: {
+			status,
+		},
+	})
 	return (
 		<>
 			<IssueActions />
